8c735fbf94514d22771e7f00e28c6efd7387abc9
bmbo123 <gregoryalan2005@gmail.com>
2024-06-20 16:24:16 -0500

    1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
-const {Client, IntentsBitField} = require('discord.js');
+require("dotenv").config();
+
+const { Client, IntentsBitField } = require("discord.js");
 
 const client = new Client({
-    intents: [
-        IntentsBitField.Flags.Guilds,
-    ]
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
 });
+
+client.on("ready", (c) => {
+  console.log(`✅${c.user.username} is ready!`);
+});
+
+client.on("messageCreate", (msg) => {});
+
+client.login(process.env.token);
